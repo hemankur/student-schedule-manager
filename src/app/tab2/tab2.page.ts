27cabfee0114066601d57cf8e7aedf52f12ca66f
hemankur <hemankur@hotmail.com>
2019-10-30 16:16:48 -0400

    Fix indentation and removed unused code

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -2,28 +2,28 @@ import {Component, OnInit} from '@angular/core';
 import {UserService} from '../services/api/user.service';
 
 @Component({
-  selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+    selector: 'app-tab2',
+    templateUrl: 'tab2.page.html',
+    styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page implements OnInit {
-  constructor (private userService: UserService) {
+    constructor(private userService: UserService) {
 
-  }
+    }
 
-  public name: string;
-  public id: number;
-  public email: string;
+    public name: string;
+    public id: number;
+    public email: string;
 
-  ngOnInit(): void {
-    this.userService.getUsers()
-        .then((res: any) => {
-          this.name = res.data[0].displayName;
-          this.email = res.data[0].email;
-          this.id = res.data[0].userID;
-          console.log(res.data[0]);
-        }).catch(err => {
-      console.log(err);
-    });
-  }
+    ngOnInit(): void {
+        this.userService.getUsers()
+            .then((res: any) => {
+                this.name = res.data[0].displayName;
+                this.email = res.data[0].email;
+                this.id = res.data[0].userID;
+                console.log(res.data[0]);
+            }).catch(err => {
+            console.log(err);
+        });
+    }
 }
