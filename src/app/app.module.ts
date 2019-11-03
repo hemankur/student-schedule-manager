b6a058e417ea76441669a9324bd170d21eb25935
hemankur <hemankur@hotmail.com>
2019-11-03 01:04:40 -0400

    Added angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -19,7 +22,10 @@ import {HttpClientModule} from '@angular/common/http';
     BrowserModule,
     IonicModule.forRoot(),
       HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+      MaterialModule,
+      ReactiveFormsModule
   ],
   providers: [
     StatusBar,
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,7 +1,29 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {UserService} from '../services/api/user.service';
 import {CoursesService} from '../services/api/courses.service';
-import {errorComparator} from 'tslint/lib/verify/lintError';
+import {MatTableDataSource} from '@angular/material/table';
+import {MatSort} from '@angular/material/sort';
+import {MatPaginator} from '@angular/material/paginator';
+
+export interface PeriodicElement {
+    name: string;
+    position: number;
+    weight: number;
+    symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+    {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+    {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+    {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+    {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+    {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+    {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+    {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+    {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+    {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+    {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
 
 @Component({
     selector: 'app-tab2',
@@ -9,15 +31,54 @@ import {errorComparator} from 'tslint/lib/verify/lintError';
     styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page implements OnInit {
-    constructor(private userService: UserService, private coursesService: CoursesService) {
 
+    constructor(private userService: UserService, private coursesService: CoursesService) {
     }
+    /*displayedColumns: string[] = ['first_name', 'last_name', 'twitter'];
+    dataSource = new MatTableDataSource<any>([
+        {
+            first_name: 'Max',
+            last_name: 'Lynch',
+            twitter: 'maxlynch'
+        },
+        {
+            first_name: 'Matt',
+            last_name: 'Netkow',
+            twitter: 'dotNetkow'
+        },
+        {
+            first_name: 'Ben',
+            last_name: 'Sperry',
+            twitter: 'benjsperry'
+        }
+    ]);*/
+    displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+    dataSource = new MatTableDataSource<any>([
+        {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+        {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+        {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+        {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+        {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+        {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+        {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+        {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+        {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+        {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'}
+    ]);
+    @ViewChild(MatPaginator) paginator: MatPaginator;
+    @ViewChild(MatSort) sort: MatSort;
 
     public name: string;
     private courseList: any;
     private filteredList: any;
 
+    applyFilter(filterValue: string) {
+        this.dataSource.filter = filterValue.trim().toLowerCase();
+    }
+
     ngOnInit(): void {
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
         this.getCourses();
         this.initializeCourses();
     }
