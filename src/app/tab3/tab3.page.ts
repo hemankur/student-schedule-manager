23eff28427b3ebdb1427767fbd5602a9b6b81abc
hemankur <hemankur@hotmail.com>
2019-11-18 12:14:28 -0500

    Added logout button

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Storage} from '@ionic/storage';
+import {NavController} from '@ionic/angular';
 
 @Component({
-  selector: 'app-tab3',
-  templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss']
+    selector: 'app-tab3',
+    templateUrl: 'tab3.page.html',
+    styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {}
+export class Tab3Page implements OnInit {
+    constructor(private storage: Storage, private navCtrl: NavController) {
+    }
+
+    logout() {
+        this.storage.set('userData', null)
+            .then(() => {
+                this.navCtrl.navigateRoot('')
+                    .catch(err => console.log(err));
+            }).catch(err => console.log(err));
+    }
+
+    ngOnInit(): void {
+    }
+}
