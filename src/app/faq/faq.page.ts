f718dd3e7194575e6d15a0557fb42c8928be0565
hemankur <hemankur@hotmail.com>
2019-11-19 00:33:09 -0500

    Search through FAQs

--- src/app/faq/faq.page.ts
+++ src/app/faq/faq.page.ts
@@ -1,21 +1,50 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {MatExpansionPanel} from '@angular/material/expansion';
-import {UserService} from '../services/api/user.service';
-import {Storage} from '@ionic/storage';
+import {MatTableDataSource} from '@angular/material/table';
+import {MatSort} from '@angular/material/sort';
 
 @Component({
-  selector: 'app-faq',
-  templateUrl: './faq.page.html',
-  styleUrls: ['./faq.page.scss'],
+    selector: 'app-faq',
+    templateUrl: './faq.page.html',
+    styleUrls: ['./faq.page.scss'],
 })
 
 export class FaqPage implements OnInit {
 
-  constructor() { }
+    constructor() {
+    }
 
-  @ViewChild(MatExpansionPanel) panel: MatExpansionPanel;
+    private faqData = [{
+        question: 'Question 1',
+        answer: 'Answer 1'
+    }, {
+        question: 'Question 2',
+        answer: 'I don\'t know'
+    }, {
+      question: 'Question 3',
+      answer: 'Make something up!'
+    }, {
+      question: 'Question 4',
+      answer: '4 Works too!'
+    }];
 
-  ngOnInit() {
-  }
+    private filterData = this.faqData;
+    dataSource = new MatTableDataSource<any>([]);
 
+    @ViewChild(MatExpansionPanel) panel: MatExpansionPanel;
+    @ViewChild(MatSort) sort: MatSort;
+
+
+    ngOnInit() {
+        this.dataSource.sort = this.sort;
+        this.dataSource.data = this.faqData;
+    }
+
+    applyFilter(filterValue: string) {
+        this.dataSource.filter = filterValue.trim().toLowerCase();
+        this.filterData = this.faqData.filter((faq) => {
+            return faq.question.toLowerCase().includes(filterValue.toLowerCase()) ||
+                faq.answer.toLowerCase().includes(filterValue.toLowerCase());
+        });
+    }
 }
