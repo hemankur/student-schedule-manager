d5559f2129307cdcd9e02eb92b62d84d98431a4a
akshayadamodaran <akshayadamodaran@gmail.com>
2019-11-20 15:47:15 -0500

    CSS Changes to login and Personal Info

--- src/app/personal/personal.page.ts
+++ src/app/personal/personal.page.ts
@@ -24,7 +24,9 @@ export class PersonalPage implements OnInit {
                     console.log(res.data);
                 });
         }).catch(err => console.log(err));
-
     }
 
+    onClickEdit() {
+
+    }
 }
